3260d50507deaf89f3210bb9e161287c040a951e
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-02-14 13:44:54 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// component & pages import
+// pages
+import Login from './pages/startLogin.js';
+import Registration from './pages/startRegistration.js';
+import Home from './pages/home.js';
+import Search from './pages/search.js';
+import Favorites from './pages/favorites.js';
+import Answers from './pages/answers.js';
+// components
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/registration" element={<Registration/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/search" element={<Search/>}/>
+          <Route path="/favorites" element={<Favorites/>}/>
+          <Route path="/answers" element={<Answers/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
