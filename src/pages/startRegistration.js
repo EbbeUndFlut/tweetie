1612dd0cef14d7ef5fe5d4939a4da30980ed656f
CryptoChris <c.peters@makermaker.de>
2022-02-16 21:52:05 +0100

    First registration implementation

--- src/pages/startRegistration.js
+++ src/pages/startRegistration.js
@@ -1,21 +1,92 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Registration = () => {
-    return (
-        <section className="start">
-            <h1>Erstelle dein Konto bei Tweetie</h1>
-            <form action="">
-                <input type="text" name="username" id="username" placeholder="Username" />
-                <input type="email" name="email" id="email" placeholder="E-Mmail" />
-                <input type="password" name="password" id="password" placeholder="Passwort" />
-                <input type="password" name="password" id="password" placeholder="Passwort Bestätigung" />
-                <input type="file" name="image" id="image" placeholder="Bild auswählen" />
-                <input className="blueButton" type="button" value="Registrieren" />
-                <Link className="greyFont" to="/">Hast du bereits ein Konto? Hier anmelden.</Link>
-            </form>
-        </section>
-    );
-}
+	const [name, setName] = useState("");
+	const [mail, setMail] = useState("");
+	const [pass, setPass] = useState("");
+	const [oassConfirm, setPassConfirm] = useState("");
+	const [pic, setPic] = useState(null);
+
+	const register = () => {
+		console.log('in register')
+		let formData = new FormData();
+		formData.append("name", name);
+		formData.append("email", mail);
+		formData.append("password", pass);
+		formData.append("avatar", pic, pic.name);
+
+		const result = fetch(
+			process.env.REACT_APP_BACKEND_URL + "/api/users",
+			{
+				method: "POST",
+				body: formData,
+			}
+		);
+	};
+
+	return (
+		<section className="start">
+			<h1>Erstelle dein Konto bei Tweetie</h1>
+			<form>
+				<input
+					onChange={(e) =>
+						setName(e.target.value)
+					}
+					type="text"
+					name="username"
+					id="username"
+					placeholder="Username"
+				/>
+				<input
+					onChange={(e) =>
+						setMail(e.target.value)
+					}
+					type="email"
+					name="email"
+					id="email"
+					placeholder="E-Mmail"
+				/>
+				<input
+					onChange={(e) =>
+						setPass(e.target.value)
+					}
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Passwort"
+				/>
+				<input
+					onChange={(e) =>
+						setPassConfirm(e.target.value)
+					}
+					type="password"
+					name="password"
+					id="password"
+					placeholder="Passwort Bestätigung"
+				/>
+				<input
+					onChange={(e) =>
+						setPic(e.target.files[0])
+					}
+					type="file"
+					name="image"
+					id="image"
+					placeholder="Bild auswählen"
+				/>
+				<input
+					onClick={register}
+					className="blueButton"
+					type="button"
+					value="Registrieren"
+				/>
+				<Link className="greyFont" to="/">
+					Hast du bereits ein Konto? Hier
+					anmelden.
+				</Link>
+			</form>
+		</section>
+	);
+};
 
 export default Registration;
