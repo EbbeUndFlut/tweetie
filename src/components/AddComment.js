aaee5d9aa3a8849bb86c7feca4323f6c7076d954
CryptoChris <c.peters@makermaker.de>
2022-02-17 15:56:21 +0100

    los gehts

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import { useState } from "react";
 
 const AddComment = () => {
-    return (  
-        <form className="addComment">
-            <input type="text" name="comment" id="comment" placeholder="Was möchtest mitteilen?" />
-            <input className="blueButton" type="submit" value="Senden" />
-        </form>
-    );
-}
- 
+	const [text, setText] = useState("");
+	const create = (e) => {
+		e.preventDefault();
+		fetch(process.env.REACT_APP_BACKEND_URL + "/api/posts", {
+			method: "POST",
+			credentials: "include",
+			body: JSON.stringify({text}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		}).then((res) => console.log(res));
+	};
+	return (
+		<form className="addComment">
+			<input
+				onChange={(e) => setText(e.target.value)}
+				type="text"
+				name="comment"
+				id="comment"
+				placeholder="Was möchtest mitteilen?"
+			/>
+			<input
+				onClick={create}
+				className="blueButton"
+				type="submit"
+				value="Senden"
+			/>
+		</form>
+	);
+};
+
 export default AddComment;
--- src/components/CommentItems.js
+++ src/components/CommentItems.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import CommentDetails from './CommentDetails.js';
-import data from '../data/data.js';
-import { v4 as uuidv4 } from 'uuid';
-
-
+import { useEffect, useState } from "react";
+import CommentDetails from "./CommentDetails.js";
+import data from "../data/data.js";
+import { v4 as uuidv4 } from "uuid";
 
 const CommentItems = () => {
-    return (
-        <section className="commentItems">
-            {
-                data.map(elt =>
-                    <CommentDetails
-                        key={uuidv4()}
-                        img={elt.img}
-                        username={elt.username}
-                        time={elt.time}
-                        comment={elt.comment}
-                        id={elt.id}
-                    />)
-            }
-        </section>
-    );
-}
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		fetch(process.env.REACT_APP_BACKEND_URL + "/api/posts", {
+			credentials: "include",
+		}).then((res) => res.json()).then(data => setPosts(data));
+	},[]);
+	return (
+		<section className="commentItems">
+			{posts.map((elt) => (
+				<CommentDetails
+					key={uuidv4()}
+					img={elt.img}
+					username={elt.creator}
+					time={elt.text}
+					comment={elt.comment}
+					id={elt.id}
+				/>
+			))}
+		</section>
+	);
+};
 
 export default CommentItems;
