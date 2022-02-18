ae8f8e0f97675c7131566f4b03ce19524fe45bd7
CryptoChris <c.peters@makermaker.de>
2022-02-18 10:23:06 +0100

    details bitches

--- src/components/CommentItems.js
+++ src/components/CommentItems.js
@@ -1,29 +1,24 @@
-import { useEffect, useState } from "react";
-import CommentDetails from "./CommentDetails.js";
-import data from "../data/data.js";
-import { v4 as uuidv4 } from "uuid";
+import { useEffect, useState } from "react"
+import CommentDetails from "./CommentDetails.js"
+import data from "../data/data.js"
+import { v4 as uuidv4 } from "uuid"
 
 const CommentItems = () => {
-	const [posts, setPosts] = useState([]);
-	useEffect(() => {
-		fetch(process.env.REACT_APP_BACKEND_URL + "/api/posts", {
-			credentials: "include",
-		}).then((res) => res.json()).then(data => setPosts(data));
-	},[]);
-	return (
-		<section className="commentItems">
-			{posts.map((elt) => (
-				<CommentDetails
-					key={uuidv4()}
-					img={elt.img}
-					username={elt.creator}
-					time={elt.text}
-					comment={elt.comment}
-					id={elt.id}
-				/>
-			))}
-		</section>
-	);
-};
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BACKEND_URL + "/api/posts", {
+            credentials: "include",
+        })
+            .then((res) => res.json())
+            .then((data) => setPosts(data))
+    }, [])
+    return (
+        <section className="commentItems">
+            {posts.map((elt) => (
+                <CommentDetails key={uuidv4()} img={elt.img} username={elt.creator} time={elt.date} comment={elt.text} id={elt.id} />
+            ))}
+        </section>
+    )
+}
 
-export default CommentItems;
+export default CommentItems
