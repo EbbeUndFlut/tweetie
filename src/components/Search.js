8329ad97e26aecaaa0225a951fa6cd3d6d6a894a
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-02-21 15:27:05 +0100

    search function vorbereitet - muss noch angepasst werden

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 import CommentDetails from './CommentDetails.js';
+import { v4 as uuidv4 } from "uuid"
 
 
 const Search = () => {
     const [allData, setAllData] = useState([]);
     const [filteredData, setFilteredData] = useState(allData);
+    const comment = (item) => {
+        allData(item)
+    }
 
     const handleSearch = (event) => {
+        let value = event.target.value.toLowerCase(); // Value ist in der Vorlage dort, wo ich unten beim map das elt habe. Verstehe nicht ganz, wie man das schreiben soll. 
+        let result = [];
+        console.log(value);
+        result = allData.filter((data) => {
+            return data.title.search(value) != -1;
+        });
+        setFilteredData(result);
     }
 
     useEffect(() => {
@@ -21,7 +32,7 @@ const Search = () => {
             .catch(error => {
                 console.log('Error getting fake data: ' + error);
             })
-    }, []);
+    }, []); // kommt hier in die [] noch was rein? 
 
     return (
         <form className="search">
