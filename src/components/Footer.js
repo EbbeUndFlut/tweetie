8409f070438af1a491424ca6897c71963766fb45
mz686 <bmiczka@web.de>
2022-02-24 21:49:41 +0100

    fck afd

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,21 +6,23 @@ import { NavLink } from 'react-router-dom'
 const Footer = () => {
     return (
         <footer>
-            <NavLink to="/home" style={({ isActive }) => ({
-                color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
-            })}>
-                <FaHouseUser />
-            </NavLink>
-            <NavLink to="/search" style={({ isActive }) => ({
-                color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
-            })}>
-                <FaSearch />
-            </NavLink>
-            <NavLink to="/favorites" style={({ isActive }) => ({
-                color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
-            })}>
-                <FaHeart />
-            </NavLink>
+            <div>
+                <NavLink to="/home" style={({ isActive }) => ({
+                    color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
+                })}>
+                    <FaHouseUser />
+                </NavLink>
+                <NavLink to="/search" style={({ isActive }) => ({
+                    color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
+                })}>
+                    <FaSearch />
+                </NavLink>
+                <NavLink to="/favorites" style={({ isActive }) => ({
+                    color: isActive ? '#0071BC' : "rgba(0, 113, 188, 0.2)",
+                })}>
+                    <FaHeart />
+                </NavLink>
+            </div>
         </footer>
     );
 }
