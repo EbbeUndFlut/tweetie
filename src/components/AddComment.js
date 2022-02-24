d2be3ca74cca7d89fc4fde1f7c03e46c1436b96e
StefanSeliger <Your_seligerstefan84@gmail.com>
2022-02-24 18:40:25 +0100

    clear addComment input field

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const AddComment = (props) => {
+	const [val, setVal] = useState();
 	const [text, setText] = useState("");
 	const create = (e) => {
 		e.preventDefault();
-		const { parentid }= props
+		const { parentid } = props
 		console.log('PROPS:', parentid)
 
 		fetch(process.env.REACT_APP_BACKEND_URL + "/api/posts", {
 			method: "POST",
 			credentials: "include",
-			body: JSON.stringify({text, parentPostId:parentid }),
+			body: JSON.stringify({ text, parentPostId: parentid }),
 			headers: {
 				"Content-Type": "application/json",
 			},
@@ -19,6 +20,7 @@ const AddComment = (props) => {
 	return (
 		<form className="addComment">
 			<input
+				value={val}
 				className="inputComment"
 				onChange={(e) => setText(e.target.value)}
 				type="text"
@@ -28,6 +30,7 @@ const AddComment = (props) => {
 			/>
 			<input
 				onClick={create}
+				onClick={() => setVal(() => "")}
 				className="blueButton"
 				type="submit"
 				value="Senden"
